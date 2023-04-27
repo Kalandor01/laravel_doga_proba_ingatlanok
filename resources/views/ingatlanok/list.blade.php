fb42ffb02eb220d3c195846266de802a369b76a5
Kalandor01 <rohovszkyakoska@gmail.com>
2023-04-27 12:41:10 +0200

    style

--- resources/views/ingatlanok/list.blade.php
+++ resources/views/ingatlanok/list.blade.php
@@ -4,18 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Ingatlanok</title>
 </head>
 <body>
-    <div>
-    {{csrf_field()}}
-    {{method_field("GET")}}
-    @foreach ($ingatlanok as $ingatlan)
-        <div>
-            <img src="img/{{$ingatlan->kepUrl}}" alt="{{$ingatlan->nev}}" onerror="this.onerror=null; this.src='img/no_image.png'">
-        </div>
-    @endforeach
+    <div id="ingatlanok">
+    <table class="table">
+        <tr>
+            <th scope="col">Kategória</th>
+            <th scope="col">Leírás</th>
+            <th scope="col">Hirdetés dátuma</th>
+            <th scope="col">Tehermentes</th>
+            <th scope="col">Fénykép</th>
+        </tr>
+        {{csrf_field()}}
+        {{method_field("GET")}}
+        @foreach ($ingatlanok as $ingatlan)
+        <tr scope="row">
+            <td><p>{{$ingatlan->nev}}</p></td>
+            <td><p>{{$ingatlan->leiras}}</p></td>
+            <td><p>{{$ingatlan->hirdetesDatuma}}</p></td>
+            <td><p>{{$ingatlan->tehermentes}}</p></td>
+            <td><img src="img/{{$ingatlan->kepUrl}}" alt="{{$ingatlan->nev}}" onerror="this.onerror=null; this.src='img/no_image.png'"></td>
+        </tr>
+        @endforeach
+    </table>
     </div>
 </body>
 </html>
--- resources/views/ingatlanok/new.blade.php
+++ resources/views/ingatlanok/new.blade.php
@@ -4,29 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Új ingatlan</title>
 </head>
 <body>
-    <form action="/api/ingatlanok", method="post">
-        {{csrf_field()}}
-        {{method_field("POST")}}
-        <select name="kategoria">
-        @foreach ($kategoriak as $kategoria)
-            <option value="{{$kategoria->id}}">{{$kategoria->nev}}</option>
-        @endforeach
-        </select><br>
-        <label for="hirdetesDatumas">Hirdetés dátuma</label><br>
-        <input type="date" name="hirdetesDatuma"><br>
-        <label for="leiras">Ingatlan leírása</label><br>
-        <input type="textarea" name="leiras"><br>
-        <input type="checkbox" name="tehermentes">
-        <label for="tehermentes">Tehermentes ingatlan</label><br>
-        <label for="ar">Ár</label><br>
-        <input type="number" name="ar" min="0"><br>
-        <label for="imgUrl">Kép az ingatlanról</label><br>
-        <input type="text" name="imgUrl"><br>
-        <input type="submit" value="Küldés">
-    </form>
+    <div id="formDiv">
+        <form action="/api/ingatlanok", method="post">
+            {{csrf_field()}}
+            {{method_field("POST")}}
+            <div class="form-group">
+            <label for="kategoria">Ingatlan kategória</label>
+            <select name="kategoria">
+            @foreach ($kategoriak as $kategoria)
+                <option value="{{$kategoria->id}}">{{$kategoria->nev}}</option>
+            @endforeach
+            </select>
+            </div>
+            <div class="form-group">
+            <label for="hirdetesDatumas">Hirdetés dátuma</label><br>
+            <input type="date" name="hirdetesDatuma"><br>
+            </div>
+            <div class="form-group">
+            <label for="leiras">Ingatlan leírása</label><br>
+            <input type="textarea" name="leiras"><br>
+            </div>
+            <div class="form-group">
+            <input type="checkbox" name="tehermentes">
+            <label for="tehermentes">Tehermentes ingatlan</label><br>
+            </div>
+            <div class="form-group">
+            <label for="ar">Ár</label><br>
+            <input type="number" name="ar" min="0"><br>
+            </div>
+            <div class="form-group">
+            <label for="imgUrl">Kép az ingatlanról</label><br>
+            <input type="text" name="imgUrl"><br>
+            </div>
+            <input type="submit" value="Küldés">
+        </form>
+    </div>
 </body>
 </html>
